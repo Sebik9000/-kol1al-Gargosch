6d7301ed54aa759f0d5a9861c496868347f20f5d
Gargosch <98969@student.vitkovickastredni.cz>
2024-11-06 11:15:31 +0100

    final_Gargosch

--- jpGargosch.py
+++ jpGargosch.py
@@ -1,1 +1,19 @@
+# 1. Pole co má 5 stringových hodnot
+pole = ["jaro", "léto", "podzim", "zima", "obdobi"]
+print("Základní pole:", pole)
 
+# 2. Přidání prvku "vítr"
+pole.append("vítr")
+print("Po přidání 'vítr':", pole)
+
+# 3. Odstranění druhého prvku z pole
+pole.remove("léto")
+print("Po odstranění druhého prvku:", pole)
+
+# 4. Změnění páté hodnoty v poli na "slunce"
+pole[4] = "slunce"
+print("Po změnění páté hodnoty na 'slunce':", pole)
+
+# 5. Přidání 2 hodnot do pole
+pole.extend(["polévka", "štrůdl"])
+print("Po přidání dvou hodnot:", pole)
